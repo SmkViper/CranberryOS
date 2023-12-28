88f9e13f33522ea468fe9571c98e6e56ddfef6ef
SmkViper <mrd_fanatic@hotmail.com>
2023-12-28 12:00:34 -0500

    More comment fixes

--- kernel/UnitTests/KernelStdlib/TypeInfoTests.cpp
+++ kernel/UnitTests/KernelStdlib/TypeInfoTests.cpp
@@ -9,15 +9,11 @@ namespace UnitTests::KernelStdlib::TypeInfo
 {
     namespace
     {
+        /**
+         * Tests for std::type_info
+         */
         void TestTypeInfo()
         {
-            //EmitTestSkipResult("type_info tests");
-            // Disabling RTTI tests for now because we've turned off rtti in the compilation options. I'd like to
-            // support RTTI at some point (and exceptions) but attempting to turn it on with -frtti (or just by having
-            // no command line arguments related to RTTI) just results in "undefined reference to typeinfo" errors from
-            // the linker. I'm guessing one of our baremetal arguments is conflicting with it and forcing rtti data
-            // generation off - or perhaps something is wrong with our implementation
-
             auto const& intTypeID1 = typeid(int);
             auto const& intTypeID2 = typeid(int);
             auto const& floatTypeID = typeid(float);
