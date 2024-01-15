157d8a4f7f4c6c24ce8a2993da41f54c26835e59
SmkViper <mrd_fanatic@hotmail.com>
2024-01-15 10:57:33 -0500

    Static analysis fixes

--- kernel/Utils.cpp
+++ kernel/Utils.cpp
@@ -1,5 +1,6 @@
 #include "Utils.h"
 
+#include <bit>
 #include <cstdint>
 #include "PointerTypes.h"
 
@@ -7,32 +8,37 @@ namespace MemoryMappedIO
 {
     void Put32(VirtualPtr const aAddress, uint32_t const aData)
     {
-        volatile auto pregister = reinterpret_cast<uint32_t*>(aAddress.GetAddress());
+        auto volatile* pregister = std::bit_cast<uint32_t*>(aAddress.GetAddress());
         *pregister = aData;
     }
 
     uint32_t Get32(VirtualPtr const aAddress)
     {
-        volatile auto pregister = reinterpret_cast<uint32_t*>(aAddress.GetAddress());
+        auto volatile* pregister = std::bit_cast<uint32_t*>(aAddress.GetAddress());
         return *pregister;
     }
 }
 
 namespace Timing
 {
-    void Delay(uint64_t aCount)
+    void Delay(uint64_t const aCount)
     {
-        while (aCount--)
+        for (auto i = 0U; i < aCount; ++i)
         {
             // make sure the compiler doesn't optimize out the decrementing so that this is
             // an actual cycle count delay
+            // NOLINTNEXTLINE(hicpp-no-assembler)
             asm volatile("nop");
         }
     }
 
     uint32_t GetSystemCounterClockFrequencyHz()
     {
+        // Lint check here is wrong, because it doesn't understand the asm output directive
+        // NOLINTNEXTLINE(misc-const-correctness)
         uint64_t frequency = 0;
+        
+        // NOLINTNEXTLINE(hicpp-no-assembler)
         asm ("mrs %0, CNTFRQ_EL0"
             :"=r"(frequency) // output
             : // no inputs
