8d1a03c93d9a3afe579cff34c0e94bebd7b3868b
SmkViper <mrd_fanatic@hotmail.com>
2024-02-03 10:47:03 -0500

    Static analysis fixes for several more simple test files

--- kernel/UnitTests/KernelStdlib/BitTests.cpp
+++ kernel/UnitTests/KernelStdlib/BitTests.cpp
@@ -8,7 +8,8 @@ namespace UnitTests::KernelStdlib::TypeTraits
         // std::bit_cast
         ///////////////////////////////////////////////////////////////////////
 
-        static_assert(std::bit_cast<int>(1.0f) == 0x3f80'0000, "Unexpected result from bit_cast");
+        // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers)
+        static_assert(std::bit_cast<int>(1.0F) == 0x3f80'0000, "Unexpected result from bit_cast");
         // #TODO: Probably more tests we could do here
         
         // #TODO: Need to find a way to test that bit_cast refuses to compile on types it can't work with
