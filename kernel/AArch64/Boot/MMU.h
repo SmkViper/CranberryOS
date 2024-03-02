f63599b29cf63e1fa258b8266b363db27cdaec0d
SmkViper <mrd_fanatic@hotmail.com>
2024-03-02 09:06:01 -0500

    Static analysis fixes for ExceptionLevel, MMU, and Output headers

--- kernel/AArch64/Boot/MMU.h
+++ kernel/AArch64/Boot/MMU.h
@@ -1,22 +1,19 @@
 #ifndef KERNEL_AARCH64_BOOT_MMU_H
 #define KERNEL_AARCH64_BOOT_MMU_H
 
-namespace AArch64
+namespace AArch64::Boot
 {
-    namespace Boot
-    {
-        /**
-         * Sets up the page tables needed for booting
-        */
-        void CreatePageTables();
+    /**
+     * Sets up the page tables needed for booting
+     */
+    void CreatePageTables();
 
-        /**
-         * Turns on the memory management unit
-        */
-        void EnableMMU();
+    /**
+     * Turns on the memory management unit
+     */
+    void EnableMMU();
 
-        // #TODO: Going to want to have a way to unmap the identity mapping once we no longer need it
-    }
+    // #TODO: Going to want to have a way to unmap the identity mapping once we no longer need it
 }
 
 #endif // KERNEL_AARCH64_BOOT_MMU_H
